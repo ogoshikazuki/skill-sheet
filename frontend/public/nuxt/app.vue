59bfcdec1d8d9fa1b296e5f74e406b377a6768b0
ogoshikazuki <k.ogoshi@altura.co.jp>
2024-01-02 17:49:05 +0900

    front

--- frontend/public/nuxt/app.vue
+++ frontend/public/nuxt/app.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>基本情報</h1>
+    <ul>
+        <li>年齢: {{ data?.basicInformation?.age }}</li>
+    </ul>
   </div>
 </template>
+
+<script lang="ts" setup>
+const query = gql`
+query {
+  basicInformation {
+    age
+  }
+}`
+
+type BasicInformationResult = {
+    basicInformation: {
+        age: number
+    }
+}
+
+const { data } = await useAsyncQuery<BasicInformationResult>(query)
+</script>
