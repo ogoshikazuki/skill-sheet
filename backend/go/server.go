ad2282d0cfe552f5d814c1bd64122fbacbdf58d3
ogoshikazuki <k.ogoshi@altura.co.jp>
2024-01-02 17:48:33 +0900

    cors

--- backend/go/server.go
+++ backend/go/server.go
@@ -8,6 +8,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/ogoshikazuki/skill-sheet/graph"
+	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
@@ -18,10 +19,15 @@ func main() {
 		port = defaultPort
 	}
 
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowCredentials: true,
+		Debug:            true,
+	})
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	http.Handle("/query", c.Handler(srv))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
