73c5060d82bf5bee8d1c2b5f6ce7f75b23df1b75
ogoshikazuki <k.ogoshi@altura.co.jp>
2024-01-12 22:12:16 +0900

    use pages

--- frontend/public/nuxt/app.vue
+++ frontend/public/nuxt/app.vue
@@ -1,28 +1,3 @@
 <template>
-  <div>
-    <h1>基本情報</h1>
-    <ul>
-      <li>生年月日: {{ data?.basicInformation?.birthday }}</li>
-    </ul>
-  </div>
+  <NuxtPage />
 </template>
-
-<script lang="ts" setup>
-// eslint-disable-next-line no-undef
-const query = gql`
-  query {
-    basicInformation {
-      birthday
-    }
-  }
-`;
-
-type BasicInformationResult = {
-  basicInformation: {
-    birthday: string;
-  };
-};
-
-// eslint-disable-next-line no-undef
-const { data } = await useAsyncQuery<BasicInformationResult>(query);
-</script>
---
+++ frontend/public/nuxt/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>基本情報</h1>
+    <ul>
+      <li>生年月日: {{ data?.basicInformation?.birthday }}</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup>
+// eslint-disable-next-line no-undef
+const query = gql`
+  query {
+    basicInformation {
+      birthday
+    }
+  }
+`;
+
+type BasicInformationResult = {
+  basicInformation: {
+    birthday: string;
+  };
+};
+
+// eslint-disable-next-line no-undef
+const { data } = await useAsyncQuery<BasicInformationResult>(query);
+</script>
