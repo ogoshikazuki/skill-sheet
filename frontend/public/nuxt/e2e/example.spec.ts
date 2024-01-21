d7befe9d0e4376a866519d2a84329561a901fc68
ogoshikazuki <ko0701s@gmail.com>
2024-01-21 20:59:07 +0900

    test

--- frontend/public/nuxt/e2e/example.spec.ts
+++ frontend/public/nuxt/e2e/example.spec.ts
@@ -1,7 +1,9 @@
 import { test, expect } from '@playwright/test'
 
 test('basic information', async ({ page }) => {
-  await page.goto('/basic-information')
+  await page.goto('/')
+
+  await page.getByRole('link', { name: '基本情報' }).click()
 
   await page.getByText('1991-07-01').waitFor()
   await expect(page.getByText('1991-07-01')).toBeVisible()
