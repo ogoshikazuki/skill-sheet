82c003ce7cb4be1975e77bd8f53fd4c56e3e243b
ogoshikazuki <ko0701s@gmail.com>
2024-01-23 00:44:28 +0900

    fix

--- frontend/public/nuxt/pages/index.vue
+++ frontend/public/nuxt/pages/index.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+const pages = [
+  {
+    path: 'basic-information',
+    label: '基本情報'
+  }
+]
+</script>
+
 <template>
-  <NuxtLink to="/basic-information">
-    基本情報
-  </NuxtLink>
+  <v-list>
+    <v-list-subheader>Menu</v-list-subheader>
+    <v-list-item v-for="page in pages" :key="page.path" @click="navigateTo(page.path)">
+      {{ page.label }}
+    </v-list-item>
+  </v-list>
 </template>
