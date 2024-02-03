f4b69d85782961d758a08a2c093c846d6dd9fe5f
ogoshikazuki <ko0701s@gmail.com>
2024-02-03 21:58:28 +0900

    external pages

--- frontend/public/nuxt/pages/index.vue
+++ frontend/public/nuxt/pages/index.vue
@@ -15,6 +15,17 @@ const pages = [
     label: 'プロジェクト経歴'
   }
 ]
+
+const externalPages = [
+  {
+    href: 'https://x.com/k_o_system',
+    label: 'X'
+  },
+  {
+    href: 'https://qiita.com/ogoshikazuki',
+    label: 'Qiita'
+  }
+]
 </script>
 
 <template>
@@ -23,6 +34,9 @@ const pages = [
       <v-list-subheader>Menu</v-list-subheader>
       <v-list-item v-for="page in pages" :key="page.path" @click="navigateTo(page.path)">
         {{ page.label }}
+      </v-list-item>
+      <v-list-item v-for="page in externalPages" :key="page.href" :href="page.href" target="_blank">
+        {{ page.label }} <v-icon icon="mdi-open-in-new" />
       </v-list-item>
     </v-list>
   </v-card>
