9dab72ca2606628473e5a40b67f657611d33a3a1
ogoshikazuki <ko0701s@gmail.com>
2024-02-04 22:15:04 +0900

    di (#43)

--- backend/go/infrastructure/server/graphql.go
+++ backend/go/infrastructure/server/graphql.go
@@ -9,24 +9,28 @@ import (
 	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/ogoshikazuki/skill-sheet/adapter/repository"
 	"github.com/ogoshikazuki/skill-sheet/config"
 	"github.com/ogoshikazuki/skill-sheet/entity"
 	"github.com/ogoshikazuki/skill-sheet/graph"
 	"github.com/ogoshikazuki/skill-sheet/infrastructure/server/middleware"
+	"github.com/ogoshikazuki/skill-sheet/usecase"
 	"github.com/vektah/gqlparser/v2/gqlerror"
 )
 
 type Server struct {
-	cfg     config.Config
-	logger  *log.Logger
-	handler http.Handler
+	cfg        config.Config
+	logger     *log.Logger
+	sqlHandler repository.SqlHandler
+	handler    http.Handler
 }
 
-func NewServer(cfg config.Config, logger *log.Logger) Server {
+func NewServer(cfg config.Config, logger *log.Logger, sqlHandler repository.SqlHandler) Server {
 	return Server{
-		cfg:     cfg,
-		logger:  logger,
-		handler: http.DefaultServeMux,
+		cfg:        cfg,
+		logger:     logger,
+		sqlHandler: sqlHandler,
+		handler:    http.DefaultServeMux,
 	}
 }
 
@@ -46,7 +50,11 @@ func (s *Server) handleHealth() {
 }
 
 func (s *Server) handleGraphQL() {
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: graph.NewResolverRoot(
+		usecase.NewFindBasicInformationUsecase(repository.NewBasicInformationRepository(s.sqlHandler)),
+		usecase.NewFindProjectUsecase(repository.NewProjectRepository(s.sqlHandler)),
+		usecase.NewSearchProjectsUsecase(repository.NewProjectRepository(s.sqlHandler)),
+	)}))
 	srv.SetErrorPresenter(func(ctx context.Context, err error) *gqlerror.Error {
 		if errors.Is(err, entity.ErrInternal) {
 			return graphql.DefaultErrorPresenter(ctx, errors.New("internal server error"))
@@ -62,7 +70,7 @@ func (s *Server) handleGraphQL() {
 func (s *Server) applyMiddleware() {
 	middlewares := [](func(http.Handler) http.Handler){
 		middleware.CORS(middleware.WithCORSAllowedOrigins(s.cfg.CORSAllowedOrigins)),
-		middleware.Dataloader(),
+		middleware.Dataloader(s.sqlHandler),
 	}
 
 	for _, middleware := range middlewares {
