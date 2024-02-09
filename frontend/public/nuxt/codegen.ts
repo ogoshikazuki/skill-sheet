79df62c820b89b646ac54990b8b4906f7ec2c2a8
ogoshikazuki <ko0701s@gmail.com>
2024-02-09 23:32:06 +0900

    move graphql schema

--- frontend/public/nuxt/codegen.ts
+++ frontend/public/nuxt/codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
-  schema: '../../../graphql/*.graphqls',
+  schema: '../../../graphql/schema/*.graphqls',
   documents: './graphql/*.graphql',
   ignoreNoDocuments: true,
   generates: {
