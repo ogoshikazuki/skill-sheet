48a321fa00d13e4ff6071e26c108e2fd16d0e461
ogoshikazuki <ko0701s@gmail.com>
2024-02-24 00:28:04 +0900

    use auth0 react

--- frontend/admin/nextjs/app/page.tsx
+++ frontend/admin/nextjs/app/page.tsx
@@ -1,19 +1,28 @@
 'use client'
-import { useUser } from '@auth0/nextjs-auth0/client'
+
+import { useAuth0 } from '@auth0/auth0-react';
 
 export default function Home() {
-  const { user, error, isLoading } = useUser();
+  const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } =
+    useAuth0();
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
-
-  if (user) {
-    return (
-      <>
-        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
-      </>
-    );
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Oops... {error.message}</div>;
   }
 
-  return <a href="/api/auth/login">Login</a>;
+  if (isAuthenticated) {
+    return (
+      <div>
+        Hello {user?.name}{' '}
+        <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+          Log out
+        </button>
+      </div>
+    );
+  } else {
+    return <button onClick={() => loginWithRedirect()}>Log in</button>;
+  }
 }
