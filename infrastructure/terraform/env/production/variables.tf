d5f21b5e1c7858f0288429384287b3eab4bcef27
ogoshikazuki <ko0701s@gmail.com>
2024-02-25 00:23:51 +0900

    fix

--- infrastructure/terraform/env/production/variables.tf
+++ infrastructure/terraform/env/production/variables.tf
@@ -7,25 +7,3 @@ variable "railway_token" {
   type      = string
   sensitive = true
 }
-
-variable "auth0_domain" {
-  type = string
-}
-
-variable "auth0_client_id" {
-  type = string
-}
-
-variable "auth0_client_secret" {
-  type      = string
-  sensitive = true
-}
-
-variable "auth0_admin_email" {
-  type = string
-}
-
-variable "auth0_admin_password" {
-  type      = string
-  sensitive = true
-}
